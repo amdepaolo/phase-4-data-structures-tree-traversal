b3e1fe842bb32eb45883518830783d4d1bbcb2bb
Adam DePaolo <amdepaolo@gmail.com>
2023-04-05 17:23:17 -0400

    commit tree eb825248c79a08835160b995755b88564c1bd97f

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,19 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id (string)
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      if node[:id] == string
+        return node
+      end
+        nodes_to_visit =  nodes_to_visit +node[:children]
+    end
+
+    return nil
+  end 
+  
 end
